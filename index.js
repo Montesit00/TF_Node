0305852bdf5e979b934f658e27987e32201ebd3f
Montesit00 <marcos.montellanok@gmail.com>
2023-04-27 15:03:53 -0300

    Pruba tf ejecutado dsd node

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const tf = require('@tensorflow/tfjs')
 
- const tensor = tf.tensor([1.1, 2.2, 3.3], null, 'float32')
-   console.log(tensor);
-   tensor.print();
+ const tensor1 = tf.tensor([1.1, 2.2, 3.3], null, 'float32')
+   console.log(tensor1);
+   tensor1.print();
    
-   const tensor2 = tf.tensor([[1,2],[3,4]]);
+  const tensor2 = tf.tensor([[1,2],[3,4]]);
    console.log(tensor2);
    tensor2.print();
+
+  const tensor3 = tf.tensor2d([[9.0, 25.0], [13.0, 34.0]]);
+   console.log(tensor3);
+   tensor3.print();
